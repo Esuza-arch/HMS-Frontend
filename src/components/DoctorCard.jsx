fa57f6fc05b353e484d41c8337616c823b2005ac
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2025-01-31 22:38:46 +0300

    Add classNames to html elements

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function DoctorCard({ doctor}) {
     return (
-        <div>
-           <div>
-            <h3>{doctor.name}</h3>
-            <p>{doctor.speciallty}</p>
-            <p>{doctor.experience} years of experience</p>
+        <div className="doctor-card">
+           <div className="doctor-info">
+            <h3 className="doctor-name">{doctor.name}</h3>
+            <p className="doctor-specialty">{doctor.speciallty}</p>
+            <p className="doctor-experience">{doctor.experience} years of experience</p>
             <Link to={`/appointments?doctorId=${doctor.id}`}>Book an appointment</Link>
            </div>
         </div>
